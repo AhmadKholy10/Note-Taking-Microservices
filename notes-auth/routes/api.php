909f5b26db156b4de2876c34f043ba5b5a2beff7
AhmedAbdelazeem98 <55737870+AhmedAbdelazeem98@users.noreply.github.com>
2023-11-26 14:14:27 +0200

    Add microservices with notes-crud as submodule

---
+++ notes-auth/routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\JwtMiddleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);  
+});
+
+Route::middleware([JwtMiddleware::class])->group(function(){
+    Route::get('/in', function(){return 'Your Token is Right' ;});
+});
